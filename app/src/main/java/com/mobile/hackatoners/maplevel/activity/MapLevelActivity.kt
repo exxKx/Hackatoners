c7e7079a47466125225246beb8d7daaf8b0e4a79
Dmitry Rubtsov <dm.mironov01@gmail.com>
2021-10-08 21:29:57 +0300

    add template screens

---
+++ app/src/main/java/com/mobile/hackatoners/maplevel/activity/MapLevelActivity.kt
@@ -0,0 +1,12 @@
+package com.mobile.hackatoners.maplevel.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapLevelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView
+    }
+}
---
+++ app/src/main/java/com/mobile/hackatoners/maplevel/fragment/ChooseRegionFragment.kt
@@ -0,0 +1,12 @@
+package com.mobile.hackatoners.maplevel.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class ChooseRegionFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
