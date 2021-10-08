4275de7427563f4d231ca117fb4bc9025990dd0c
Dmitry Rubtsov <dm.mironov01@gmail.com>
2021-10-08 21:36:07 +0300

    Update theme, add MapLevelActivity

--- app/src/main/java/com/mobile/hackatoners/maplevel/activity/MapLevelActivity.kt
+++ app/src/main/java/com/mobile/hackatoners/maplevel/activity/MapLevelActivity.kt
@@ -2,11 +2,12 @@ package com.mobile.hackatoners.maplevel.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mobile.hackatoners.R
 
 class MapLevelActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView
+        setContentView(R.layout.activity_maplevel)
     }
 }
