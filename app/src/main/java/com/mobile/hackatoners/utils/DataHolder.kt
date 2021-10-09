d28f8bcb8dc065a33aec84b53898b188483853fd
Dmitry Rubtsov <dm.mironov01@gmail.com>
2021-10-09 14:30:51 +0300

    Add dialog

--- app/src/main/java/com/mobile/hackatoners/utils/DataHolder.kt
+++ app/src/main/java/com/mobile/hackatoners/utils/DataHolder.kt
@@ -23,6 +23,10 @@ class DataHolder private constructor(
         get() = sharedPreferences.getInt(KEY_HILL_LEVEL, 1)
         set(value) = sharedPreferences.edit().putInt(KEY_HILL_LEVEL, value).apply()
 
+    var isMapTutorialComplete: Boolean
+        get() = sharedPreferences.getBoolean(KEY_MAP_TUTORIAL_COMPLETE, false)
+        set(value) = sharedPreferences.edit().putBoolean(KEY_MAP_TUTORIAL_COMPLETE, value).apply()
+
     var isRealWorldUnlocked: Boolean
         get() = sharedPreferences.getBoolean(KEY_REAL_WORLD_UNLOCKED, false)
         set(value) = sharedPreferences.edit().putBoolean(KEY_REAL_WORLD_UNLOCKED, value).apply()
@@ -37,6 +41,7 @@ class DataHolder private constructor(
         private const val KEY_DESERT_LEVEL = "desert_level"
         private const val KEY_HILL_LEVEL = "hill_level"
 
+        private const val KEY_MAP_TUTORIAL_COMPLETE = "is_map_tutorial_complete"
         private const val KEY_REAL_WORLD_UNLOCKED = "is_real_world_unlocked"
 
         private var dataHolder: DataHolder? = null
