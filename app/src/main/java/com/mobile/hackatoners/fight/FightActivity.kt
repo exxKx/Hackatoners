973ed614201e81e926819cae11e57565098de374
Samarkin Mikhail <mikle.samarkin@gmail.com>
2021-10-09 21:49:34 +0300

    inflation and unboard

--- app/src/main/java/com/mobile/hackatoners/fight/FightActivity.kt
+++ app/src/main/java/com/mobile/hackatoners/fight/FightActivity.kt
@@ -11,21 +11,29 @@ import kotlinx.android.synthetic.main.activity_fight.*
 import android.animation.Animator
 
 import android.animation.AnimatorListenerAdapter
+import android.content.Intent
 import android.view.View
 
 import android.view.animation.DecelerateInterpolator
+import com.mobile.hackatoners.results.ResultsActivity
+import com.mobile.hackatoners.results.ResultsActivity.Companion.ANSWERS_COUNT
+import com.mobile.hackatoners.results.ResultsActivity.Companion.ELAPSED_TIME
+import com.mobile.hackatoners.results.ResultsActivity.Companion.INFLATION
+import com.mobile.hackatoners.results.ResultsActivity.Companion.IS_VICTORY
+import com.mobile.hackatoners.results.ResultsActivity.Companion.RIGHT_ANSWERS
 import com.mobile.hackatoners.utils.DataHolder
 import com.mobile.hackatoners.utils.Region
 
 import com.yy.mobile.rollingtextview.CharOrder.Number
 import com.yy.mobile.rollingtextview.strategy.Direction
 import com.yy.mobile.rollingtextview.strategy.Strategy
+import kotlinx.coroutines.cancel
 
 
 class FightActivity : AppCompatActivity(), FightListener {
 
     val fightViewModel: FightViewModel by viewModels()
-    
+
     private val region by lazy { Region.find(intent.getIntExtra(REGION, Region.FOREST.value)) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +47,7 @@ class FightActivity : AppCompatActivity(), FightListener {
         val display = windowManager?.defaultDisplay
         val size = Point()
         display?.getSize(size)
-        fight_scene.createPlayers(size.x,size.y)
+        fight_scene.createPlayers(size.x, size.y)
         fight_scene.listenerFight = this
         rolling_text.animationDuration = 500L
         rolling_text.charStrategy = Strategy.CarryBitAnimation(Direction.SCROLL_DOWN)
@@ -69,6 +77,8 @@ class FightActivity : AppCompatActivity(), FightListener {
                 }
 
             }
+            if (it < 1)
+                startResultActivity(victory = false)
         }
 
         fightViewModel.bossHP.observe(this) {
@@ -81,29 +91,31 @@ class FightActivity : AppCompatActivity(), FightListener {
                 }
 
             }
+            if (it < 1)
+                startResultActivity(victory = true)
         }
         fightViewModel.money.observe(this) {
             rolling_text.setText(it.toString())
         }
         fightViewModel.currentState.observe(this) {
             it?.let {
-                when(it){
-                    FightState.ONBOARD_1 ->{
+                when (it) {
+                    FightState.ONBOARD_1 -> {
                         unboard_1.visibility = View.VISIBLE
                         unboard_2.visibility = View.GONE
                         unboard_3.visibility = View.GONE
                     }
-                    FightState.ONBOARD_2 ->{
+                    FightState.ONBOARD_2 -> {
                         unboard_1.visibility = View.GONE
                         unboard_2.visibility = View.VISIBLE
                         unboard_3.visibility = View.GONE
                     }
-                    FightState.ONBOARD_3 ->{
+                    FightState.ONBOARD_3 -> {
                         unboard_1.visibility = View.GONE
                         unboard_2.visibility = View.GONE
                         unboard_3.visibility = View.VISIBLE
                     }
-                    FightState.FIGHT ->{
+                    FightState.FIGHT -> {
                         unboard_1.visibility = View.GONE
                         unboard_2.visibility = View.GONE
                         unboard_3.visibility = View.GONE
@@ -121,15 +133,46 @@ class FightActivity : AppCompatActivity(), FightListener {
         unboard_3.setOnClickListener {
             fightViewModel.updateOnboarding()
         }
-
-
-        when (region) {
-            Region.FOREST -> fight_scene.setBackgroundResource(R.drawable.stage_forest)
-            Region.DESERT -> fight_scene.setBackgroundResource(R.drawable.stage_desert)
-            Region.WORLD -> Unit // never used
-            Region.HILL -> fight_scene.setBackgroundResource(R.drawable.stage_hill)
+        fightViewModel.inflationEvent.observe(this) {
+            animateShow()
         }
     }
+
+    private fun animateShow() {
+        inflation_text?.apply {
+            this.visibility = View.VISIBLE
+            alpha = 0f
+            animate().alpha(1f).setDuration(1000).setInterpolator(DecelerateInterpolator()).setUpdateListener {
+                if (it.animatedValue == 1f)
+                    animateHide()
+            }.start()
+
+        }
+    }
+
+    private fun animateHide() {
+        inflation_text?.apply {
+            alpha = 1f
+            animate().alpha(0f).setDuration(1000).setInterpolator(DecelerateInterpolator()).setUpdateListener {
+                if (it.animatedValue == 1f)
+                    this.visibility = View.GONE
+            }.start()
+
+        }
+    }
+
+    fun startResultActivity(victory: Boolean) {
+        fightViewModel.scope.cancel()
+        Intent(this, ResultsActivity::class.java).run {
+            putExtra(IS_VICTORY, victory)
+            putExtra(ANSWERS_COUNT, fightViewModel.answerCount)
+            putExtra(RIGHT_ANSWERS, fightViewModel.rightAnswerCount)
+            putExtra(ELAPSED_TIME, fightViewModel.timeOnBoss)
+            putExtra(INFLATION, fightViewModel.inflationSpent)
+            startActivity(this)
+        }
+    }
+
 
     override fun updateLifeCount(player: Boolean) {
         if (player) {
