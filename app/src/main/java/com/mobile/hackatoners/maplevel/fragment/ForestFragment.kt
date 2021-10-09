932a2108bc32c40a1f94683ac9d0f55599d5bca4
Dmitry Rubtsov <dm.mironov01@gmail.com>
2021-10-09 13:42:46 +0300

    Forest & Hill locations

--- app/src/main/java/com/mobile/hackatoners/maplevel/fragment/ForestFragment.kt
+++ app/src/main/java/com/mobile/hackatoners/maplevel/fragment/ForestFragment.kt
@@ -1,12 +1,66 @@
 package com.mobile.hackatoners.maplevel.fragment
 
+import android.animation.ValueAnimator
+import android.graphics.BitmapFactory
+import android.graphics.Shader
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.view.animation.LinearInterpolator
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.mobile.hackatoners.R
+import com.mobile.hackatoners.utils.DataHolder
 
-class ForestFragment : Fragment() {
+class ForestFragment : Fragment(R.layout.fragment_forest) {
+
+    private val dataHolder by lazy { DataHolder.getInstance(requireContext()) }
+
+    private lateinit var backgroundOne: View
+    private lateinit var backgroundTwo: View
+    private lateinit var forest: ImageView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        backgroundOne = view.findViewById(R.id.background_one)
+        backgroundTwo = view.findViewById(R.id.background_two)
+        forest = view.findViewById(R.id.forest)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_water_tile)
+        val tiledImage = BitmapDrawable(resources, bitmap).also {
+            it.setTileModeXY(
+                Shader.TileMode.REPEAT,
+                Shader.TileMode.REPEAT
+            )
+        }
+        backgroundOne.background = tiledImage
+        backgroundTwo.background = tiledImage
+
+        ValueAnimator.ofFloat(0.0f, 1.0f).apply {
+            repeatCount = ValueAnimator.INFINITE
+            interpolator = LinearInterpolator()
+            duration = 20000L
+            addUpdateListener { animation ->
+                val progress = animation.animatedValue as Float
+                val width = backgroundOne.width.toFloat()
+                val translationX = width * progress
+                backgroundOne.translationX = translationX
+                backgroundTwo.translationX = translationX - width
+            }
+        }.start()
+
+        forest.startAnimation(
+            AnimationUtils.loadAnimation(requireContext(), R.anim.float_animation_up)
+        )
+
+        val image = when (dataHolder.hillLevel) {
+            1 -> R.drawable.bg_forest_1
+            2 -> R.drawable.bg_forest_2
+            3 -> R.drawable.bg_forest_3
+            else -> R.drawable.bg_forest_4
+        }
+        forest.setImageResource(image)
     }
 }
