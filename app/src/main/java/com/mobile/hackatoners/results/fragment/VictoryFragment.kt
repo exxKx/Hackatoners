397271f86a04507a60ce4b50dc802668778d7054
Dmitry Rubtsov <dm.mironov01@gmail.com>
2021-10-09 18:35:29 +0300

    Add empty victory fragment

---
+++ app/src/main/java/com/mobile/hackatoners/results/fragment/VictoryFragment.kt
@@ -0,0 +1,13 @@
+package com.mobile.hackatoners.results.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.mobile.hackatoners.R
+
+class VictoryFragment : Fragment(R.layout.fragment_victory) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
